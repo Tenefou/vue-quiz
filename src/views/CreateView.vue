7c8dc87e6da3d071e64a630b94ae4dd32850f06f
Gaspar RIVOIRE-GAIAO <gaspar.rivoire-gaiao@groupama.com>
2025-02-14 15:09:48 +0100

    create page start

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -2,15 +2,53 @@
   <HeaderComponent />
   <main>
     <h1>Create Quiz</h1>
-    <form>
-      <input type="text" id="title" name="title" />
-      <button type="submit">Create</button>
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="title">Titre</label>
+        <input type="text" id="title" name="title" />
+      </div>
+      <div class="form-group">
+        <label for="title">Question {{ numberQuestion }}</label>
+        <input type="text" id="title" name="title" />
+        <label for="">Réponse 1</label>
+        <input type="text" id="title" name="title" />
+        <label for="">Réponse 2</label>
+        <input type="text" id="title" name="title" />
+        <label for="">Réponse 3</label>
+        <input type="text" id="title" name="title" />
+      </div>
+      <div style="display: flex; gap: 3vh">
+        <button @click="handleAdd">Ajouter question</button>
+        <button type="submit">Créer quiz</button>
+      </div>
     </form>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import HeaderComponent from '../components/HeaderComponent.vue'
+
+const numberQuestion = ref(1)
+
+const handleAdd = () => {
+  numberQuestion.value++
+}
+
+const submitForm = () => {
+  console.log('submitForm')
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 5vh;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+}
+</style>
